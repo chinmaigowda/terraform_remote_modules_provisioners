1ec60add34820b4f2d43e3b4a1aa8ed7309e7298
chinmaigowda <chinmaigowda45@gmail.com>
2024-06-11 00:59:59 +0530

    5

--- file/variables.tf
+++ file/variables.tf
@@ -1,3 +1,15 @@
+variable "user_name" {
+  type = string
+  default = ""
+}
+variable "ec2_public_ip" {
+  type = string
+  default = ""
+}
+variable "ec2_pem_path" {
+  type = string
+  default = ""
+}
 variable "source_file_path" {
   type = string
   default = ""
